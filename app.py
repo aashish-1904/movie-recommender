b185abb91f9617c1477d5fa6e9d26e1e904c4e1f
aashish-1904 <akumar7_be20@thapar.edu>
2022-11-12 17:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import bz2file as bz2
 
 def decompress_pickle(file):
-        data = bz2.BZ2File(file, ‘rb’)
+        data = bz2.BZ2File(file, 'rb')
         data = pickle.load(data)
         return data
 
